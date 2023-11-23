98c4eb2ed109e2c3c685a44004045cade1981db7
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-23 15:45:19 +0800

    Add files via upload

---
+++ Solidity Tutorial/18_RevertErrorHandler.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract RevertStatement {
+
+    function checkOverflow(
+        uint256 _number1,
+        uint256 _number2 
+    )
+        public pure returns
+    (
+        string memory
+    )
+    {
+        uint256 sum = _number1 + _number2;
+        if(sum < 0 || sum > 255) {
+            revert ("Overflow exist");
+        } else {
+            return ("No Overflow");
+        }
+    }
+}
