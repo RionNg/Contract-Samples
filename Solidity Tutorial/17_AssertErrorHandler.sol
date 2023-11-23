c65d9419a596677ff34879b521ada542db78021f
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-23 15:44:57 +0800

    Add files via upload

---
+++ Solidity Tutorial/17_AssertErrorHandler.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract AssertStatement {
+
+    bool result;
+
+    function checkOverflow(
+        uint256 _number1,
+        uint256 _number2
+    ) public {
+        uint256 sum = _number1 + _number2;
+        assert(sum <= 255);
+        result = true;
+    }
+
+    function getResult() public view returns(string memory) {
+        if(result == true) {
+            return "No overflow";
+        } else {
+            return "Overflow exist";
+        }
+    }
+}
