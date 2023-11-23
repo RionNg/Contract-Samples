3208b54ca23bef487762630925c4d5b7d101b3d7
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-23 15:44:37 +0800

    Add files via upload

---
+++ Solidity Tutorial/16_RequireErrorHandler.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Require {
+
+    // Using "require" statement to check the condition
+    function checkInput(
+        uint256 _input
+    )
+        public pure returns
+    (
+        string memory
+    )
+    {
+        require(_input >= 0, "Invalid uint8");
+        require(_input <= 255, "Invalid uint256");
+        return "Input is Uint8";
+    }
+
+    function Odd(uint256 _input) public pure returns(bool) {
+        require(_input % 2 != 0);
+        return true;
+    }
+}
