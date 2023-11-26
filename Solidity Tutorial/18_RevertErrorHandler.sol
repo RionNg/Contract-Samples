acf4e8995ba69c4c234b1e71d66ae0cd52624206
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-26 15:46:42 +0800

    18_RevertErrorHandler.sol

--- Solidity Tutorial/18_RevertErrorHandler.sol
+++ Solidity Tutorial/18_RevertErrorHandler.sol
@@ -4,14 +4,8 @@ pragma solidity ^0.8.0;
 
 contract RevertStatement {
 
-    function checkOverflow(
-        uint256 _number1,
-        uint256 _number2 
-    )
-        public pure returns
-    (
-        string memory
-    )
+    function checkOverflow(uint256 _number1, uint256 _number2)
+        public pure returns(string memory)
     {
         uint256 sum = _number1 + _number2;
         if(sum < 0 || sum > 255) {
