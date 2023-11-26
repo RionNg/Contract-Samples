80e54671779aed3f446e16d3f68b6e99fb30fe71
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-26 15:49:47 +0800

    08_View.sol

--- Solidity Tutorial/08_View.sol
+++ Solidity Tutorial/08_View.sol
@@ -30,8 +30,9 @@ contract View {
     // }
 
     function getTotal()
-        public view returns
-    (uint256 product, uint256 total)
+        public
+        view
+        returns (uint256 product, uint256 total)
     {
         product = firstNumber * secondNumber;
         total = firstNumber + secondNumber;
